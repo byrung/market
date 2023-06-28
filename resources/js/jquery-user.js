c0608dea6829b0314f6a73573c8d2ccdde1f50fd
byrung <kl78411@navere.com>
2023-06-29 03:19:22 +0900

    1

--- resources/js/jquery-user.js
+++ resources/js/jquery-user.js
@@ -40,8 +40,6 @@ $(document).ready(function() {
 
 	}).scroll();
 
-
-
 });
 
 function goTop(){ 
@@ -50,26 +48,3 @@ function goTop(){
 	// 이 소스가 동작하지 않는다면
 	// $('html, body')로 변경
 }
-$(document).ready(function() {
-
-	// 기존 css에서 플로팅 배너 위치(top)값을 가져와 저장한다.
-	var floatPosition = parseInt($("#top_btn").css('top'));
-	// 250px 이런식으로 가져오므로 여기서 숫자만 가져온다. parseInt( 값 );
-
-	$(window).scroll(function() {
-		// 현재 스크롤 위치를 가져온다.
-		var scrollTop = $(window).scrollTop();
-		var newPosition = scrollTop + floatPosition + "px";
-
-		 $("#top_btn").css('top', newPosition);
-
-
-        /* 애니메이션 효과를 받으며 따라감.
-         $("#top_btn").stop().animate({
-			"top" : newPosition
-		}, 400); */
-
-	}).scroll();
-
-
-});
